9f4daef563258c9074da2f1f8e5744a59c28f743
Mohamed Elsmokhraty <90973364+moElsmokhraty@users.noreply.github.com>
2021-11-17 23:50:00 +0200

    finished splash screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:islami/Home/HomeScreen.dart';
+import 'package:islami/SplashScreen.dart';
 
 void main() {
   runApp(MyApp());
+
 }
 
 class MyApp extends StatelessWidget {
+
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      home: ,
+
+      routes: {
+        SplashScreen.ROUTE_NAME: (context)=> SplashScreen(),
+        HomeScreen.ROUTE_NAME: (context)=> HomeScreen()
+      },
+      initialRoute: SplashScreen.ROUTE_NAME,
     );
   }
 }
